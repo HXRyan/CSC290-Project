ea17943b37f5f1cf342a85f25215c0153258cc13
Chencen Liang <coco.liang@mail.utoronto.ca>
2019-02-11 13:19:38 -0500

    add Interfaces for major assets
    - added interface for vessel classes
    
    - added interface for projectile classes

---
+++ Galaxy Invaders/Assets/Script/IVessels.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IVessels{
+
+    void Move();
+
+    void Attack();
+
+    int GetX();
+
+    int GetY();
+
+    int GetWidth();
+
+    int GetHeight();
+    
+}
